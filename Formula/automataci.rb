8b841f61b42d2c9ce822e5b0e6d42b81cc964793
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2023-10-10 21:48:56 +0800

    automation: publish as of automataci 1.6.0

--- Formula/automataci.rb
+++ Formula/automataci.rb
@@ -3,10 +3,15 @@ class AutomataCI < Formula
   homepage "https://your-product.website.here"
   license "Apache-2.0"
   url "https://github.com/ChewKeanHo/AutomataCI/releases/download/1.6.0/automataci-homebrew_1.6.0_any-any.tar.xz"
-  sha256 "62954c24275ca84d58eb19e1094d4440e8f2cbf6a891b4e14b7df4b1e137c0e5"
+  sha256 "38ad8a2539f2d0c4f747ab8b673257800459d11401069063bb080cd4a9e0449e"
 
+  on_linux do
+    depends_on "gcc" => [:build, :test]
+  end
 
-  depends_on "go" => [:build, :test]
+  on_macos do
+    depends_on "clang" => [:build, :test]
+  end
 
   def install
     system "./ci.cmd setup"
@@ -14,6 +19,7 @@ class AutomataCI < Formula
     system "./ci.cmd materialize"
     chmod 0755, "bin/automataci"
     bin.install "bin/automataci"
+    libexec.install Dir["lib/*"]
   end
 
   test do
