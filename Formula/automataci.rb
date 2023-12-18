1f4a449822f4dc6b444eeeaac5a2ad55e952c195
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2023-12-18 17:11:15 +0800

    automation: published as of automataci 1.7.0

--- Formula/automataci.rb
+++ Formula/automataci.rb
@@ -3,7 +3,7 @@ class AutomataCI < Formula
   homepage "https://your-product.website.here"
   license "Apache-2.0"
   url "https://github.com/ChewKeanHo/AutomataCI/releases/download/1.7.0/automataci-homebrew_1.7.0_any-any.tar.xz"
-  sha256 "b5c016b00b14aefe876788f9102431c387a45b504ee540fb9602acfc05b3f5f6"
+  sha256 "864e63f8d0a43e9e5084ba2314cdcfdd85603a43a531b36fdb1971d6d5f503b5"
 
 
   def install
